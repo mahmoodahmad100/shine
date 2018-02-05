bcaf257c38d1cfae785b61f3a0198a072fffdeca
mahmoodahmad100 <mahmood073@gmail.com>
2018-02-05 22:52:16 +0200

    not null constraint

--- db/migrate/20180205204424_create_customers.rb
+++ db/migrate/20180205204424_create_customers.rb
@@ -1,12 +1,12 @@
 class CreateCustomers < ActiveRecord::Migration[5.0]
   def change
     create_table :customers do |t|
-      t.string :first_name
-      t.string :last_name 
-      t.string :email
-      t.string :username
-      
-      t.timestamps
+      t.string :first_name , null: false
+      t.string :last_name , null: false
+      t.string :email , null: false
+      t.string :username , null: false
+
+      t.timestamps , null: false
     end
   end
 end
