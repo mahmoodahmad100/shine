e263da9e0a16061a512509dd7aee9f26f0ae98b8
mahmoodahmad100 <mahmood073@gmail.com>
2018-02-16 09:14:31 +0200

    setting up build_for_name_search() with all helpers it needs

--- app/models/customer_search_term.rb
+++ app/models/customer_search_term.rb
@@ -22,14 +22,18 @@ private
 	def build_for_name_search(search_term)
 		@where_clause << case_insensitive_search(:first_name)
 		@where_args[:first_name] = starts_with(search_term)
+		@where_clause << " OR #{case_insensitive_search(:last_name)}"
+		@where_args[:last_name] = starts_with(search_term)
+
+		@order = "last_name asc"
 	end
 
 	def starts_with(search_term)
-		
+		search_term + "%"	
 	end
 
 	def case_insensitive_search(field_name)
-		
+		"lower(#{field_name}) like: #{field_name}"
 	end
 
 end
