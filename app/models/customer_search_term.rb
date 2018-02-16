5b416db2a2677915d8e0a2187975ad1c4bbd062b
mahmoodahmad100 <mahmood073@gmail.com>
2018-02-16 09:37:50 +0200

    much better

--- app/models/customer_search_term.rb
+++ app/models/customer_search_term.rb
@@ -19,10 +19,6 @@ private
 
 	end
 
-	def extract_name(email)
-		email.gsub(/@.*$/,"").gsub(/[0-9]+/,"") #fsdaf456
-	end
-
 	def build_for_name_search(search_term)
 		@where_clause << case_insensitive_search(:first_name)
 		@where_args[:first_name] = starts_with(search_term)
@@ -30,6 +26,10 @@ private
 		@where_args[:last_name] = starts_with(search_term)
 
 		@order = "last_name asc"
+	end
+
+	def extract_name(email)
+		email.gsub(/@.*$/,"").gsub(/[0-9]+/,"") #fsdaf456
 	end
 
 	def starts_with(search_term)
