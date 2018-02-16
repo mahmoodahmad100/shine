5b77c6b2607987d96a1b53752fdd8e2b526c6138
mahmoodahmad100 <mahmood073@gmail.com>
2018-02-16 09:49:42 +0200

    setting up build_for_email_search()

--- app/models/customer_search_term.rb
+++ app/models/customer_search_term.rb
@@ -16,12 +16,22 @@ class CustomerSearchTerm
 private
 	
 	def build_for_email_search(search_term)
+		@where_clause << case_insensitive_search(:first_name)
+		@where_args[:first_name] = starts_with(extract_name(search_term))
 
+		@where_clause << " OR #{case_insensitive_search(:last_name)}"
+		@where_args[:last_name] = starts_with(extract_name(search_term))
+
+		@where_clause << " OR #{case_insensitive_search(:email)}"
+		@where_args[:email] = search_term
+
+		@order = "lower(email) = " + ActiveRecord::Base.connection.quote(search_term) + " desc, last_name asc"
 	end
 
 	def build_for_name_search(search_term)
 		@where_clause << case_insensitive_search(:first_name)
 		@where_args[:first_name] = starts_with(search_term)
+
 		@where_clause << " OR #{case_insensitive_search(:last_name)}"
 		@where_args[:last_name] = starts_with(search_term)
 
