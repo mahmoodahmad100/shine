30b8c70bae1750fd18f2275262d7087a73ca6ad1
mahmoodahmad100 <mahmood073@gmail.com>
2018-03-04 10:17:30 +0200

    clean code & fixing a bug

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,17 @@
 require "rails_helper"
+require 'support/violate_check_constraint_matcher'
 
 describe User do
 	describe "email" do
 
 		let(:user){
-			User.create!("email": "hello@example.com", "password": "fdfdfdfd5", "password_confirmation": "fdfdfdfd5")
+			User.create!("email": "hello@example.com", "password": "fdfdfdfd530", "password_confirmation": "fdfdfdfd530")
 		}
 
 		it "prevents invalid email addresses" do
 			expect{
 				user.update_attribute(:email, "hello@test.com")
-			}.to raise_error(ActiveRecord::StatementInvalid,/email_must_be_company_email/i)
+			}.to violate_check_constraint(:email_must_be_company_email)
 		end
 	end
 end
