690e0cc659dbc929a1e7b935b36610868db7a972
mahmoodahmad100 <mahmood073@gmail.com>
2018-04-05 22:45:07 +0200

    barely done

--- spec/javascripts/CustomerSearchComponent.spec.js
+++ spec/javascripts/CustomerSearchComponent.spec.js
@@ -1,4 +1,23 @@
 describe("CustomerSearchComponent", function() {
-
-
+	describe("initial state", function() {
+		it("sets customers to null");
+		it("sets keywords to the empty string");
+	});
+	describe("search", function() {
+		describe("A search for 'pa' ... less than 3 characters", function() {
+			it("sets the keywords to be 'pa'");
+			it("does not make an HTTP call");
+		});
+		describe("A search for 'pat', ... 3 or +3 characters", function() {
+			describe("A successful search", function() {
+				it("sets the keywords to be 'pat'");
+				it("sets the customers to the results of the HTTP call");
+			});
+			describe("A search that fails", function() {
+				it("sets the keywords to be 'pat'");
+				it("leaves customers as null");
+				it("alerts the user with the response message");
+			});
+		});
+	});
 });
