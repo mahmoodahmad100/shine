17afc9a143f6e94f7b98adcb3f27d3fd151485a5
mahmoodahmad100 <mahmood073@gmail.com>
2018-05-03 23:07:59 +0200

    small changes

--- spec/javascripts/CustomerSearchComponent.spec.js
+++ spec/javascripts/CustomerSearchComponent.spec.js
@@ -54,13 +54,27 @@ describe("CustomerSearchComponent", function() {
 			beforeEach(function() {
 				var response = td.object(["json"]);
 				td.when(response.json()).thenReturn({ customers: customers });
+
+		        var observable = td.object(["subscribe"]);
+		        td.when(observable.subscribe(
+		        	td.callback(response),
+		          	td.matchers.isA(Function))
+		        ).thenReturn();
+
 				mockHttp = td.object(["get"]);
+				td.when(mockHttp.get("/customers.json?keywords=pat")).thenReturn(observable);
 				component = new CustomerSearchComponent(mockHttp);
 			});
 			
 			describe("A successful search", function() {
-				it("sets the keywords to be 'pat'");
-				it("sets the customers to the results of the HTTP call");
+				it("sets the keywords to be 'pat'",function(){
+					component.search("pat");
+					expect(component.keywords).toBe("pat");
+				});
+				it("sets the customers to the results of the HTTP call",function(){
+					component.search("pat");
+					expect(component.customers).toBe(customers);
+				});
 			});
 			describe("A search that fails", function() {
 				it("sets the keywords to be 'pat'");
