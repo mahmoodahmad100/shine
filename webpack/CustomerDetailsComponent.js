1992b2a6891f3c3f8e9b38e5122f8460b2c96040
mahmoodahmad100 <mahmood073@gmail.com>
2018-05-08 14:12:34 +0200

    getting the id param

--- webpack/CustomerDetailsComponent.js
+++ webpack/CustomerDetailsComponent.js
@@ -1,7 +1,8 @@
 var reflectMetadata = require("reflect-metadata");
 
 var ng = {
-	core: require("@angular/core")
+	core: require("@angular/core"),
+	router: require("@angular/router")
 };
 
 var CustomerDetailsComponent = ng.core.Component({
@@ -10,10 +11,21 @@ var CustomerDetailsComponent = ng.core.Component({
 })
 .Class({
 	constructor:[
-		function(){
-
+		ng.router.ActivatedRoute,
+		function(activatedRoute){
+			this.activatedRoute = activatedRoute;
+			this.id = null;
 		}
-	]
+	],
+	ngOnInit: function() {
+		var self = this;
+		self.activatedRoute.params.subscribe(
+			function(params){
+				var id = +params['id'];
+				self.id = id;
+			}
+		);
+	}
 });
 
 module.exports = CustomerDetailsComponent;
