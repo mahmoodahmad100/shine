502c8207dfa040a1d212a04dd2ea6b82eeda6c23
mahmoodahmad100 <mahmood073@gmail.com>
2018-05-18 05:46:47 +0200

    yeah! getting the customer details from customers & other DB tables

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,9 +7,9 @@ class CustomersController < ApplicationController
 	end
 
   def show
-    customer = Customer.find(params[:id])
+    customer_detail = CustomerDetail.find(params[:id])
     respond_to do |format|
-      format.json { render json: { customer: customer } }
+      format.json { render json: { customer: customer_detail } }
     end
   end
 
